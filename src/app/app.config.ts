50a81cc49b095c6308dd24c979bb6c876ccb23c6
raulgar19 <raulgamu2003@gmail.com>
2025-11-07 14:17:32 +0100

    fix: add comment to clarify usage of withFetch in HttpClient configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), //para fetch pasar por parámetro withFetch()
   ],
 };
